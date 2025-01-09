00c572be4ba62debd3c4f1a8d96286ca9c8cd773
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2025-01-09 14:41:04 +0100

    feat: router in app.jsx completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppLayout from "./component/AppLayout";
+import HomePage from "./component/HomePage";
+import ChiSiamo from "./component/ChiSiamo";
+import PostsList from "./component/PostsList";
+
 function App() {
-  
   return (
-    <>
-    </>
-  )
- 
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<HomePage/>} />
+          <Route path="/ChiSiamo" element={<ChiSiamo />} />
+          <Route path="/PostsList" element={<PostsList />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
-
-
+export default App;
---
+++ src/component/HomePage.jsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+
+    return (
+        <main>
+            <h1>HomePage</h1>
+        </main>
+    )
+}
+
+export default HomePage;
